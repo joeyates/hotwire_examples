23f7f154945cbb816f39eac9fee183779728c541
Joe Yates <joe.g.yates@gmail.com>
2024-04-17 11:50:29 +0200

    Show edit form for todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,16 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:edit]
+
   def index
     @todos = Todo.all
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
   end
 end
